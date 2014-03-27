da46a96051f99a3dfd1cf312a638f90904f8709a
JordiSanchezMarin <jordisanchez00@gmail.com>
2014-03-27 11:26:59 +0100

    Mastermind CSS
    falta controlar el codigo por el input

--- MasterMind2/masterui.js
+++ MasterMind2/masterui.js
@@ -13,15 +13,18 @@
 
     añadirPista: function () {
         // añade la ultima pista al array de pistas
-        $("#historial_pistas").append("<div class='row clearfix' id='his_"+ config.intentosNow +"'></div>");
+        $("#historial_pistas").append("<div class='row clearfix historial' id='his_"+ config.intentosNow +"'></div>");
         for( i = 0 ; i<5 ; i++){
-			$("#his_" + config.intentosNow).append("<div class='col-md-2 column'>" +
+			$("#his_" + config.intentosNow).append("<div class='col-md-1 column'>" +
                         "<div class='circulo " + master.codigoPrueba.getArrayColor()[i] +"'>" +
 
                         "</div>" +
 				"</div>");	
 		}
-		$("#his_" + config.intentosNow).append($("#pistas>div").clone().attr("id", "pistas" + config.intentosNow));
+		$("#his_" + config.intentosNow).append("<div class='col-md-2 column'>" +
+               config.intentosNow +
+		"</div>");	
+		$("#his_" + config.intentosNow).append($("#pistas>div").clone().attr("id", "pistas" + config.intentosNow) );
     },
 
     borrarPistas: function () {
@@ -32,7 +35,7 @@
 	añadirColores: function(id_div){
 		//añade los cuadros de colores para elegir
 		for( i = 0 ; i<5 ; i++){
-			$("#" + id_div).append("<div id='i_" + i + "' class='col-md-2 column' style='text-align:center;padding-top:4px'> " +
+			$("#" + id_div).append("<div id='i_" + i + "' class='col-md-2 column' > " +
                     "<div id='blue_" + i + "' class='cuadrado blue'>0</div><div id='green_" + i + "' class='cuadrado green'>1</div><div id='red_" + i + "' class='cuadrado red'>2</div>" +
                     "<div id='gold_" + i + "' class='cuadrado gold'>3</div><div id='blueviolet_" + i + "' class='cuadrado blueviolet'>4</div><div id='black_" + i + "' class='cuadrado black'>5</div>" +
 			"</div>");	
@@ -41,6 +44,7 @@
 	
 	añadirResultado: function(id_div){
 		//añade los 5 circulos que varian segun la opcion elegida
+		
 		for( i = 0 ; i<5 ; i++){
 			$("#" + id_div).append("<div class='col-md-2 column'>" +
                         "<div id='r_" + i + "' class='circulo'>" +
