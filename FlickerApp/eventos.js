fe76b8115a01cec636c4a86f9d5a8c963b38dc7b
JordiSanchezMarin <jordisanchez00@gmail.com>
2014-05-13 13:49:08 +0200

    carga de imagenes flickr
    Falta CSS

--- FlickerApp/eventos.js
+++ FlickerApp/eventos.js
@@ -1,33 +1,32 @@
 $(document).ready(function(){
 	$("#btn_enviar").click(function(){
-		$.getJSON("http://api.flickr.com/services/rest/?&method=flickr.people.getPublicPhotos&api_key=5c9026d4cfbf2ef9b29747d878da99c8&user_id=12403504@N02", 
-		displayImages);
-	
-	
+		var vartag = "";
+		var varid = "";
+		if($("#txtTag").val()!=""){
+		    vartag=$("#txtTag").val().trim();
+		}
+		if($("#txtId").val()!=""){
+		    varid=$("#txtId").val().trim();
+		}
+		$("#div_imagenes").empty();
+		$.ajax({
+            url: "https://api.flickr.com/services/feeds/photos_public.gne?format=json&id="+varid+"&tags="+vartag+"&jsoncallback=?",
+            dataType: "json",
+            complete: function(jqXHR, textStatus) {
+            	
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+            	
+            },
+            success: function(data) {
+            	for (var i=0 ;i<data.items.length;i++) {
+					$("#div_imagenes").append("<div class='div_imagen' id='img"+i+"'></div>");
+					$("#img"+i).append("<img  src='"+data.items[i].media.m+"'/>");
+		            $("#img"+i).append("<p>ID = "+data.items[i].author_id+"</p>");
+		            $("#img"+i).append("<p>TAGS = "+data.items[i].tags+"</p>");
+				}
+            }
+        });
 	});
-	function displayImages(data) {
-	
-	    // Start putting together the HTML string
-	    var htmlString = "a";
-	    
-	    // Now start cycling through our array of Flickr photo details
-	    $.each(data.items, function(i,item){
-	    
-	        // I only want the ickle square thumbnails
-	        var sourceSquare = (item.media.m).replace("_m.jpg", "_s.jpg");
-	        
-	        // Here's where we piece together the HTML
-	        htmlString += '<li><a href="' + item.link + '" target="_blank">';
-	        htmlString += '<img title="' + item.title + '" src="' + sourceSquare;
-	        htmlString += '" alt="'; htmlString += item.title + '" />';
-	        htmlString += '</a></li>';
-	    
-	    });
-	    
-	    // Pop our HTML in the #images DIV
-	    $('#div_imaganes').html(htmlString);
-	    
-	    // Close down the JSON function call
-	}
 });
 
